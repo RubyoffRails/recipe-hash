41e6f43c0ae60143bb27109dc3e38b8ec39ae262
Kyle Martin <KyleMartin901@gmail.com>
2013-07-26 00:26:26 +1000

    Change recipe from food to trains

--- recipe.rb
+++ recipe.rb
@@ -1,17 +1,24 @@
-ingredients = {}
-ingredients[:avocados] = 4
-ingredients[:jalapenos] = 2
-
-Recipe = Struct.new(:ingredients, :method)
-
-recipe = Recipe.new( {avacados: 4, jalapenos: 2}, ["Peel / Slice Avocados", "Chop jalapenos into small dice"])
-
-puts "ingredients"
-recipe.ingredients.each do |key, value|
-	puts "* #{key}: #{value}"
+# Train Struct
+Train = Struct.new(:current_city, :num_engines, :num_cars, :caboose) do	
+	# Print details of the train
+	def to_s
+		puts "\sCurrent city: #{current_city}"
+		puts "\sNumber of engines: #{num_engines}"
+		puts "\sNumber of cars: #{num_cars}"
+		puts "\sCaboose: #{caboose}"
+	end
 end
 
-puts "\nMethod"
-recipe.method.each_with_index do |step, index|
-	puts "#{index+1}. #{step}"
-end
+# Passanger Struct
+Passanger = Struct.new(:name, :train)
+
+# Create train and passanger
+train = Train.new( "Sydney", 2, 8, true)
+passanger= Passanger.new( "Kyle", train )
+
+# Print passangers name
+puts "Passanger\n\s#{passanger.name}"
+
+# Print passangers train details
+puts "\nTrain"
+puts "#{passanger.train.to_s}"
