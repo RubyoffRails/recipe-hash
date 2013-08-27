47fbf6c6657df580261bd9ba411ea2b07a4047e0
lorail <lori@loriortiz.com>
2013-08-27 17:30:51 -0400

    Add passenger3 and new train

--- train.rb
+++ train.rb
@@ -3,11 +3,10 @@ train[:city] = "New York"
 train[:engines] = 1
 train[:cars] = 6
 
-train_parts = {}
-train_parts[:train] = train
 
 puts "Train:"
-train_parts[:train].each do |key, value|
+
+train.each do |key, value|
 	puts " * #{key}: #{value}"
 end
 
