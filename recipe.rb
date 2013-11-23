8d36154328ea51ff7f9396255a95c024768a7942
Maurice Womack <womackm@gmail.com>
2013-11-23 17:51:54 -0500

    HW 2
    Thanks.

--- recipe.rb
+++ recipe.rb
@@ -1,17 +1,17 @@
-ingredients = {}
-ingredients[:avocados] = 4
-ingredients[:jalapenos] = 2
+train = {}
+train[:engines] = 4
+train[:current_city] = "Columbus"
+train[:number_of_cars] = 15
+train[:caboose] = true
 
-Recipe = Struct.new(:ingredients, :method)
+Passenger = Struct.new(:train, :name)
 
-recipe = Recipe.new( {avacados: 4, jalapenos: 2}, ["Peel / Slice Avocados", "Chop jalapenos into small dice"])
+passenger = Passenger.new({engines: 4, current_city: "Columbus", number_of_cars: 2, caboose: true}, ["Maurice Womack"])
 
-puts "ingredients"
-recipe.ingredients.each do |key, value|
+puts "train information"
+passenger.train.each do |key, value|
 	puts "* #{key}: #{value}"
 end
 
-puts "\nMethod"
-recipe.method.each_with_index do |step, index|
-	puts "#{index+1}. #{step}"
-end
+puts "\nName:"
+puts passenger.name
