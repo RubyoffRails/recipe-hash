de734c43c8f480b7ecfb7d4bb30886092ee811e5
L Lumbreras <llumbreras@gmail.com>
2013-12-02 16:23:57 -0800

    Change train from hash to struct

--- recipe.rb
+++ recipe.rb
@@ -1,13 +1,11 @@
-train = {city: "Santa Fe", engines: 2, cars: 5, caboose: "Yes"}
+Train = Struct.new(:city,:engines, :cars, :caboose)
+train = Train.new("Santa Fe", 2, 5, "Yes")
 
 Passenger = Struct.new(:name, :train)
-
 passenger = Passenger.new( "Jimmy Dean", train ) 
 
 puts "\nTrain"
-train.each do |key, value|
-	puts "* #{key.capitalize}: #{value}"
-end
+puts "* City: #{train.city}\n* Engines: #{train.engines}\n* Cars: #{train.cars}\n* Caboose: #{train.caboose}"
 
 puts "\nPassenger"
 puts "#{passenger.name} on train to #{passenger.train[:city]}. "
