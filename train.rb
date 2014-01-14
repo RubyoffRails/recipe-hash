3cf9361b1ae1c2fe25535460809890e5fa1a0e80
Rob Jewell <jewell.robertp@gmail.com>
2014-01-14 12:25:08 -0500

    Add Train Object and Update Printed Output

--- train.rb
+++ train.rb
@@ -2,13 +2,15 @@ Passenger = Struct.new(:name)
 Train = Struct.new(:destination, :number_of_cars, :caboose, :number_of_engine)
 
 passengers = [Passenger.new(name: 'Bill Smith'), Passenger.new(name: 'Sarah Robot')]
-Train.new( passengers, {destination: 'New York', number_of_engines: 4, caboose: true, number_of_cars: 5})
+train = Train.new( passengers, {destination: 'New York', number_of_engines: 4, caboose: true, number_of_cars: 5})
 
 puts "Destination: #{train.destination}"
 puts "Cars: #{train.number_of_cars}"
 puts "Caboose: #{train.caboose}"
-puts "Passengers: #{train.passengers}"
+puts "Passengers: #{passengers}"
 puts "Engines: #{train.number_of_engines}"
+
+
 
 =begin
 puts 'train specifications:'
