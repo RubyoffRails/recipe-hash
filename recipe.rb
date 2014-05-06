e54f9fca2eba1b174d5a0461395e5f516f4bf6b2
dnourie2 <dananourie@gmail.com>
2014-05-05 19:05:13 -0700

    Removed puts from class def, and added it where I call the method.

--- recipe.rb
+++ recipe.rb
@@ -1,6 +1,6 @@
 Train = Struct.new(:city, :engines, :cars, :caboose) do
   def info
-    puts "This train is going to #{city}, and it has #{engines} engines, #{cars} cars, and #{caboose} caboose."
+    "This train is going to #{city}, and it has #{engines} engines, #{cars} cars, and #{caboose} caboose."
   end
 end
 
@@ -8,13 +8,13 @@ amtrack = Train.new("San Jose", 2, 8, 1)
 
 Passenger = Struct.new(:name, :train) do
   def trip
-   puts "#{name} is riding the #{train} while on vacation."
+   "#{name} is riding the #{train} while on vacation."
   end
 end
 
 first_passenger = Passenger.new("Sharon", "amtrack")
-first_passenger.trip
-amtrack.info
+puts first_passenger.trip
+puts amtrack.info
 #amtrack.each_pair {|name, value| puts("About the train: #{name} => #{value}")}
 #The above line is not necessary, but I thought it was cool:-)
 
